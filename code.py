9652a820ce95f9c46999852015d1e464974efba4
Yoda-Flash <kaitlyn.zee@gmail.com>
2026-06-19 00:43:36 -0700

    I should've added the TFT screen code!

--- code.py
+++ code.py
@@ -1,42 +1,62 @@
-import board, digitalio, analogio, busio, audiobusio, synthio
-import adafruit_74hc595
-from midi_tools import note_or_name
 import math
+
+import board
+import terminalio
+from displayio import Group, release_displays
+from adafruit_display_text import label
+from adafruit_74hc595 import ShiftRegister74HC595
+from adafruit_st7735 import ST7735
+from analogio import AnalogIn, AnalogOut
+from audiobusio import I2SOut
+from digitalio import DigitalInOut, Direction
+from fourwire import FourWire
+from synthio import Synthesizer
+
+from midi_tools import note_or_name
 
 board_voltage = 3.3
 voltage_tolerance = 0.5
 
 # Shift register init
-sr_latch_pin = digitalio.DigitalInOut(board.D9)
-sr = adafruit_74hc595.ShiftRegister74HC595(board.SPI(), sr_latch_pin)
+sr_latch_pin = DigitalInOut(board.D9)
+sr = ShiftRegister74HC595(board.SPI(), sr_latch_pin)
 
 select_pins = [sr.get_pin(pin) for pin in range(1, 5)]
 for pin in select_pins:
-    pin.direction = digitalio.Direction.OUTPUT
+    pin.direction = Direction.OUTPUT
 
 # Analog multiplexer init
 
-mux_pin = analogio.AnalogIn(board.A0)
+mux_pin = AnalogIn(board.A0)
 # mux_pins = ["1", "3", "5", "7", "9", "11", "13", "row0", "col0", "col1", "col2", "row1", "row2", "loop", "invert", "octave"]
 
 # Inputs init
 row_pins = [7, 11, 12]
 col_pins = [8, 9, 10]
-keys = [["i", "ii", "iii"], ["iv", "v", "vi"], ["vii", "keyup", "keydown"]]
-chord_labels = ["i", "ii", "iii", "iv", "v", "vi", "vii"]
+keys = [["I", "II", "III"], ["IV", "V", "VI"], ["VII", "keyup", "keydown"]]
+chord_labels = ["I", "II", "III", "IV", "V", "VI", "VII"]
 pressed_keys = []
 
-pot_voltage_bounds = [[0.0, 0.5], [0.6, 1.0], [1.2, 1.8], [2.0, 2.6], [2.8, 3.3]] # Fill with actual voltage max/min boundary at each marking when testing the physical board
+pot_voltage_bounds = [[0.0, 0.5], [0.6, 1.0], [1.2, 1.8], [2.0, 2.6], [2.8, 3.3]] # TODO: Fill with actual voltage max/min boundary at each marking when testing the physical board
 
 invert_pin = 14
 octave_pin = 13
 loop_pin = 15
 
+# TFT screen init
+release_displays()
+
+display_bus = FourWire(board.SPI(), command=board.D2, chip_select=board.D7)
+display = ST7735(display_bus, width=160, height=80, rotation=180)
+
+splash = Group()
+text_group = Group(scale=2, x=10, y=10) # TODO: Figure out a good offset for the words when testing physical board
+
 # DAC for speaker init
-audio = audiobusio.I2SOut(board.D5, board.D1, board.D4)
+audio = I2SOut(board.D5, board.D1, board.D4)
 
 # Synth for playing music
-synth = synthio.Synthesizer(sample_rate=44100)
+synth = Synthesizer(sample_rate=44100)
 audio.play(synth)
 
 # Processing variables init
@@ -45,6 +65,37 @@ current_key = note_or_name(f"C{current_octave}")
 root_note = current_key
 rel_notes = []
 notes = []
+
+CHORD_NAMES = {
+    "maj": [0, 4, 7],
+    "min": [0, 3, 7],
+    "aug": [0, 4, 8],
+    "dim": [0, 3, 6],
+    "sus": [0, 5, 7],
+    "sus2": [0, 2, 7],
+    "6": [0, 4, 7, 9],
+    "(add2), (add9)": [0, 2, 4, 7],
+    "maj7": [0, 4, 7, 11],
+    "7": [0, 4, 7, 10],
+    "7♭5": [0, 4, 6, 10],
+    "7sus": [0, 5, 7, 10],
+    "m(add2), m(add9)": [0, 2, 3, 7],
+    "m6": [0, 3, 7, 9],
+    "m7": [0, 3, 7, 10],
+    "m(maj7)": [0, 3, 7, 11],
+    "m7♭5": [0, 3, 6, 10],
+    "dim7": [0, 3, 6, 9],
+    "5": [0, 7],
+    "maj7♯11": [0, 4, 7, 11, 18],
+    "maj9": [0, 4, 7, 11, 14],
+    "7♯9": [0, 4, 7, 10, 15],
+    "9": [0, 4, 7, 10, 14],
+    "11, 9(sus4)": [0, 7, 10, 14, 17],
+    "13": [0, 4, 7, 10, 14, 21],
+    "m9": [0, 3, 7, 10, 14],
+    "m11": [0, 3, 7, 10, 17]
+}
+
 chord_sequence = []
 timer = 0
 time_sequence = []
@@ -125,6 +176,25 @@ def next_loop_pos(pos, len):
     return pos
 
 
+def get_chord_notes(notes):
+    return "  ".join(notes)
+
+
+def get_chord_name(rel_notes):
+    reordered_notes = [note + 12 if note < 0 else note for note in rel_notes]
+
+    for name, notes in CHORD_NAMES.items():
+        if notes == reordered_notes:
+            return f" - {name}"
+    return ""
+
+
+def get_base_note(notes):
+    if notes[0] != 0:
+        return f" / {note_or_name(notes[0])}"
+    else:
+        return ""
+
 while True:
     # Read from Analog Multiplexer first
     pin_readings = []
@@ -136,13 +206,13 @@ while True:
 
         if pin in row_pins:
             mux_pin.deinit()
-            mux_pin = analogio.AnalogOut(board.A0)
+            mux_pin = AnalogOut(board.A0)
             mux_pin.value = 0
             for col in col_pins:
                 set_select_pins(col, select_pins)
 
                 mux_pin.deinit()
-                mux_pin = analogio.AnalogIn(board.A0)
+                mux_pin = AnalogIn(board.A0)
                 if abs(get_analog_voltage(mux_pin)-board_voltage) <= voltage_tolerance:
                     pressed_keys.append(keys[pin][col])
 
@@ -189,6 +259,11 @@ while True:
 
             notes = [int(note + root_note) for note in rel_notes]
             synth.press(notes)
+
+            text = f"{note_or_name(current_key)}{get_chord_name(rel_notes)}{get_base_note(notes)} \n {get_chord_notes(notes)}"
+            text_label = label.Label(terminalio.FONT, text=text)
+            text_group.append(text_label)
+            splash.append(text_group)
 
             if is_recording_loop:
                 if timer != 0:
