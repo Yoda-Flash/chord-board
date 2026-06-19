5790c7c86429b0406da61bfb0d31148b9224939d
Yoda-Flash <kaitlyn.zee@gmail.com>
2026-06-18 23:36:38 -0700

    Finished all input code

--- code.py
+++ code.py
@@ -1,5 +1,10 @@
-import board, digitalio, analogio, busio
+import board, digitalio, analogio, busio, audiobusio, synthio
 import adafruit_74hc595
+from midi_tools import note_or_name
+import math
+
+board_voltage = 3.3
+voltage_tolerance = 0.5
 
 # Shift register init
 sr_latch_pin = digitalio.DigitalInOut(board.D9)
@@ -9,16 +14,45 @@ select_pins = [sr.get_pin(pin) for pin in range(1, 5)]
 for pin in select_pins:
     pin.direction = digitalio.Direction.OUTPUT
 
-mux_pin = analogio.AnalogIn(board.A0)
+# Analog multiplexer init
 
-mux_pins = ["P1", "P2", "P3", "P4", "P5", "P6", "P7", "ROW0", "COL0", "COL1", "COL2", "ROW1", "ROW2"]
+mux_pin = analogio.AnalogIn(board.A0)
+# mux_pins = ["1", "3", "5", "7", "9", "11", "13", "row0", "col0", "col1", "col2", "row1", "row2", "loop", "invert", "octave"]
+
+# Inputs init
 row_pins = [7, 11, 12]
 col_pins = [8, 9, 10]
-keys = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+keys = [["i", "ii", "iii"], ["iv", "v", "vi"], ["vii", "keyup", "keydown"]]
+chord_labels = ["i", "ii", "iii", "iv", "v", "vi", "vii"]
 pressed_keys = []
 
+pot_voltage_bounds = [[0.0, 0.5], [0.6, 1.0], [1.2, 1.8], [2.0, 2.6], [2.8, 3.3]] # Fill with actual voltage max/min boundary at each marking when testing the physical board
+
+invert_pin = 14
+octave_pin = 13
+loop_pin = 15
+
+# DAC for speaker init
+audio = audiobusio.I2SOut(board.D5, board.D1, board.D4)
+
+# Synth for playing music
+synth = synthio.Synthesizer(sample_rate=44100)
+audio.play(synth)
+
+# Processing variables init
+current_octave = 4
+current_key = note_or_name(f"C{current_octave}")
+root_note = current_key
+rel_notes = []
+notes = []
+chord_sequence = []
+timer = 0
+time_sequence = []
+is_recording_loop = False
+loop_pos = 0
+
 def get_analog_voltage(mux_pin):
-    return mux_pin.value / 65536
+    return (mux_pin.value / 65536)*board_voltage
 
 
 def number_to_bits(number):
@@ -33,14 +67,70 @@ def bits_to_booleans(bits: str):
     return booleans
 
 
+def set_select_pins(pin, select_pins):
+    booleans = bits_to_booleans(number_to_bits(pin))
+    i = 0
+    while i < 4:
+        select_pins[i].value = booleans[i]
+        i += 1
+
+
+def raise_key(key):
+    key += 1
+    if key > note_or_name(f"C{current_octave+1}"):
+        key - 12
+    return key
+
+
+def lower_key(key):
+    key -= 1
+    if key < note_or_name(f"C{current_octave-1}"):
+        key + 12
+    return key
+
+
+def get_rel_note_num_from_index(index):
+    return index*2
+
+
+def get_note_mod_from_index(index):
+    return index - 2
+
+
+def invert_chord(notes, voltage):
+    num_inversions = math.floor(board_voltage - voltage)/(board_voltage/len(notes)) # Assuming linear voltage by slider's distance, divide voltage by the proportion of the slider's distance
+    if num_inversions == 0:
+        return notes
+    shifted_notes = [(note - 12) for note in notes[-num_inversions:]]
+    del notes[-num_inversions:]
+    return shifted_notes + notes
+
+
+def cycle_octave(octave):
+    octave += 1
+    if octave > 6:
+        octave = 2
+    return octave
+
+
+def update_key(key):
+    keystring = note_or_name(key)[:1] # Isolates the key without the octave designation
+    return note_or_name(f"{keystring}{current_octave}")
+
+
+def next_loop_pos(pos, len):
+    pos += 1
+    if pos >= len:
+        pos = 0
+    return pos
+
+
 while True:
+    # Read from Analog Multiplexer first
     pin_readings = []
+    pressed_keys = []
     for pin in range(0, 16):
-        booleans = bits_to_booleans(number_to_bits(pin))
-        i = 0
-        while i < 4:
-            select_pins[i].value = booleans[i]
-            i += 1
+        set_select_pins(pin, select_pins)
 
         pin_readings.append(get_analog_voltage(mux_pin))
 
@@ -49,14 +139,64 @@ while True:
             mux_pin = analogio.AnalogOut(board.A0)
             mux_pin.value = 0
             for col in col_pins:
-                booleans = bits_to_booleans(number_to_bits(pin))
-                i = 0
-                while i < 4:
-                    select_pins[i].value = booleans[i]
-                    i += 1
+                set_select_pins(col, select_pins)
 
                 mux_pin.deinit()
                 mux_pin = analogio.AnalogIn(board.A0)
-                if mux_pin.value == 1:
+                if abs(get_analog_voltage(mux_pin)-board_voltage) <= voltage_tolerance:
                     pressed_keys.append(keys[pin][col])
 
+    pot_readings = pin_readings[:7]
+    for reading_index, reading in enumerate(pot_readings):
+        for bound_index, bound in enumerate(pot_voltage_bounds):
+            if bound[0] <= reading <= bound[1]:
+                note = get_rel_note_num_from_index(reading_index)
+                mod = get_note_mod_from_index(bound_index)
+                rel_notes.append(note + mod)
+                break
+
+    invert_chord(rel_notes, pin_readings[invert_pin])
+
+    if abs(pin_readings[octave_pin] - board_voltage) < voltage_tolerance:
+        current_octave = cycle_octave(current_octave)
+        current_key = update_key(current_key)
+
+    if abs(pin_readings[loop_pin] - board_voltage) < voltage_tolerance:
+        if not is_recording_loop: #Reset at the beginning of each record
+            chord_sequence.clear()
+            time_sequence.clear()
+            timer = 0
+            loop_pos = 0
+            is_recording_loop = True
+        timer += 1
+    else:
+        if is_recording_loop:
+            time_sequence.append(timer)
+            timer = 0
+            is_recording_loop = False
+        if timer > time_sequence[loop_pos]:
+            synth.release(chord_sequence[loop_pos])
+            loop_pos = next_loop_pos(loop_pos, len(time_sequence))
+            synth.press(chord_sequence[loop_pos])
+
+        timer += 1
+
+    for key in pressed_keys:
+        if key in chord_labels:
+            root_note = current_key + chord_labels.index(key)
+            synth.release(notes)
+            notes.clear()
+
+            notes = [int(note + root_note) for note in rel_notes]
+            synth.press(notes)
+
+            if is_recording_loop:
+                if timer != 0:
+                    time_sequence.append(timer)
+                    timer = 0
+                chord_sequence.append(notes)
+
+        elif key == "keyup":
+            current_key = raise_key(current_key)
+        elif key == "keydown":
+            current_key = lower_key(current_key)
---
+++ midi_tools.py
@@ -0,0 +1,66 @@
+# Copied from https://github.com/CedarGroveStudios/CircuitPython_MIDI_Tools/blob/main/cedargrove_midi_tools.py
+
+# Note names used by note_or_name, note_name, and name_note helpers
+NOTE_BASE = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+
+def note_or_name(value):
+    """Bidirectionally translates a MIDI sequential note value to a note name
+    or a note name to a MIDI sequential note value. Note values are integers in
+    the range of 0 to 127 (inclusive). Note names are character strings
+    expressed in the format NoteOctave such as 'C4' or 'G#7'. Note names range
+    from 'C-1' (note value 0) to 'F#9' (note value 127). If the input value is
+    outside the note value or name range, the value of `None` is returned.
+
+    :param union(int, str) value: The note name or note value input. Note value
+    is an integer Note name is a string. No default value.
+    """
+    if isinstance(value, str):
+        # Input is a string, so it's a note name
+        return name_to_note(value)
+    if isinstance(value, int):
+        # Input is an integer, so it's a note value
+        return note_to_name(value)
+    return None  # Invalid input parameter type
+
+
+def note_to_name(note):
+    """Translates a MIDI sequential note value to a note name. Note values are
+    integers in the range of 0 to 127 (inclusive). Note names are character
+    strings expressed in the format NoteOctave such as 'C4' or 'G#7'. Note
+    names range from 'C-1' (note value 0) to 'F#9' (note value 127). If the
+    input value is outside that range, the value of `None` is returned.
+
+    :param int note: The note value input in the range of 0 to 127 (inclusive).
+    No default value.
+    """
+    if 0 <= note <= 127:
+        return NOTE_BASE[note % 12] + str((note // 12) - 1)
+    return None  # Note value outside valid range
+
+
+def name_to_note(name):
+    """Translates a note name to a MIDI sequential note value. Note names are
+    character strings expressed in Scienfic Pitch Notation (NoteOctave) format
+    such as 'C4' or 'G#7' with middle C defined as 'C4'. Note names range from
+    'C-1' (note value 0) to 'G9' (note value 127). Note values are of integer
+    type in the range of 0 to 127 (inclusive). If the input value is outside
+    that range, the value of `None` is returned.
+
+    :param str name: The note name input in SPN format. No default value.
+    """
+    name = name.upper()  # Convert lower to uppercase
+    if "-" in name:
+        octave = int(name[-2:])
+        name = name[:-2]
+    else:
+        octave = int(name[-1:])
+        name = name[:-1]
+
+    if name in NOTE_BASE:
+        # Note name is valid
+        note = NOTE_BASE.index(name)
+        midi_note = note + (12 * (octave + 1))  # MIDI note value
+        if 0 <= midi_note <= 127:
+            return midi_note
+    return None  # Name is invalid or outside MIDI value range
